3ab9182186cfff5f31e95e1912c94e3b2fb4736e
Tina <skielred@gmail.com>
2019-11-05 18:43:03 +0100

    add grid system

--- keysmaniac/game.py
+++ keysmaniac/game.py
@@ -1,13 +1,18 @@
+from pyglet import gl
 from pyglet.window import Window
 from pyglet.app import run
 
+from .display import Grid
 from .log import logging
 from .scenes.play import PlayScene
 
 class Game():
 
     def __init__(self):
-        self.window = Window()
+        gl.glEnable(gl.GL_TEXTURE_2D)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_NEAREST)
+        self.window = Window(width=640, height=360)
+        Grid.set_factor_from_resolution(*self.window.get_size())
         self.window.push_handlers(self)
         self.scene = None
 
@@ -24,3 +29,10 @@ class Game():
             return
         self.window.clear()
         self.scene.draw()
+
+    def on_resize(self, width, height):
+        Grid.set_factor_from_resolution(width, height)
+        self.scene.resize()
+
+    def on_activate(self):
+        self.on_draw()
