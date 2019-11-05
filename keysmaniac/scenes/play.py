3717712316a5bfdac0672d19d8a9f0cac46a64a4
Tina <skielred@gmail.com>
2019-11-05 18:46:13 +0100

    display simple notes from a chart in PlayScene

--- keysmaniac/scenes/play.py
+++ keysmaniac/scenes/play.py
@@ -1,15 +1,34 @@
+from pyglet import clock
+
 from ..scene import Scene
 from ..objects import note_img
+from ..display import Grid
+
 
 class PlayScene(Scene):
-    def __init__(self):
+
+    def scroll(self, delta):
+        self.time += delta
+        print(self.time)
+
+    def load(self):
+        self.time = 0
+        self.scroll_speed = 100
+        clock.schedule_interval(self.scroll, 1 / 120)
+        print(clock.get_fps())
         self.simple_note = note_img
+        if 'chart' in self.context:
+            self.chart = self.context['chart']
+            print(self.chart)
+
+    def resize(self):
+        self.simple_note.width, self.simple_note.height = Grid(3, 1)
 
     def draw(self):
-        c = range(5, 1000, 20)
-        b = range(5, 1000, 20)
-        for y in range(1, 20, 8):
-            for x in range(1, 5):
-                self.simple_note.blit(c[x], b[x + y])
-            for x in range(1, 4):
-                self.simple_note.blit(c[4 - x], b[x + y + 4])
+        Grid.draw_grid()
+        keys = [0, 3, 6, 9]
+        for note in self.chart:
+            self.simple_note.blit(*Grid(
+                3 + keys[note.key - 1], 1,
+                offset_y=(note.beat.to_time() - self.time) * self.scroll_speed
+            ))
